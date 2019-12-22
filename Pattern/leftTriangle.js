fdd984b145ea6cdd86717f727b09938ab32c7f97
lgope <ilakshmangope@gmail.com>
2019-12-22 23:17:52 +0600

    Pattern Example 🗽❄

---
+++ Pattern/leftTriangle.js
@@ -0,0 +1,3 @@
+for (let i = 0, space = 5; i < 6; i++, space--) {
+    console.log(' '. repeat(space) +'*'. repeat(i));
+}
---
+++ Pattern/rightTriangle.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 6; i++) {
+    console.log('*'. repeat(i));
+}
