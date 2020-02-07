6f516694fbf916c1205b80a05c30a4c9dba61133
lgope <ilakshmangope@gmail.com>
2020-02-07 23:55:54 +0600

    Secure Password Generator 🔐🔐

---
+++ Password-Generator/READme.md
@@ -0,0 +1,7 @@
+# Password Generator
+
+It's generate high secure password.
+
+## Deploy
+
+https://lgope-password-generator.netlify.com/
--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,11 +1,11 @@
-const characterAmountRange = document.getElementById("characterAmountRange");
-const characterAmountNumber = document.getElementById("characterAmountNumber");
-const includeUppercaseElement = document.getElementById("includeUppercase");
-const includeNumbersElement = document.getElementById("includeNumbers");
-const includeSymbolsElement = document.getElementById("includeSymbols");
+const characterAmountRange = document.getElementById('characterAmountRange');
+const characterAmountNumber = document.getElementById('characterAmountNumber');
+const includeUppercaseElement = document.getElementById('includeUppercase');
+const includeNumbersElement = document.getElementById('includeNumbers');
+const includeSymbolsElement = document.getElementById('includeSymbols');
 
-const form = document.getElementById("passwordGeneratorForm");
-const passwordDisplay = document.getElementById("passwordDisplay");
+const form = document.getElementById('passwordGeneratorForm');
+const passwordDisplay = document.getElementById('passwordDisplay');
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
@@ -15,10 +15,10 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47)
   .concat(arrayFromLowToHigh(91, 96))
   .concat(arrayFromLowToHigh(123, 126));
 
-characterAmountNumber.addEventListener("input", syncCharacterAmount);
-characterAmountRange.addEventListener("input", syncCharacterAmount);
+characterAmountNumber.addEventListener('input', syncCharacterAmount);
+characterAmountRange.addEventListener('input', syncCharacterAmount);
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   const characterAmount = characterAmountNumber.value;
   const includeUppercase = includeUppercaseElement.checked;
@@ -51,7 +51,7 @@ function generatePassword(
       charCodes[Math.floor(Math.random() * charCodes.length)];
     passwordCharacters.push(String.fromCharCode(characterCode));
   }
-  return passwordCharacters.join("");
+  return passwordCharacters.join('');
 }
 
 function arrayFromLowToHigh(low, high) {
