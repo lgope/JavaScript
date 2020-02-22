f783009b0f8453ed16fc15146c1d756b191bbe20
lgope <ilakshmangope@gmail.com>
2020-02-22 17:22:15 +0600

    JavaScript Coding Techniques.. 🚀

---
+++ js-coding-technique/decimal-base.js
@@ -0,0 +1,15 @@
+// Longhand:
+
+for (let i = 0; i < 10000; i++) {}
+
+// Shorthand:
+
+for (let i = 0; i < 1e7; i++) {}
+
+// All the below will evaluate to true
+1 === 1;
+1e1 === 10;
+1e2 === 100;
+1e3 === 1000;
+1e4 === 10000;
+1e5 === 100000;
---
+++ js-coding-technique/object-property.js
@@ -0,0 +1,9 @@
+// Longhand:
+
+const x = 1920,
+  y = 1080;
+const obj = { x: x, y: y };
+
+// Shorthand:
+
+const obj = { x, y }; // es6 features
