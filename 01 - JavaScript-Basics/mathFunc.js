86af303a818aaedc8b8ed2aa56a27914a10bb2b6
lgope <ilakshmangope@gmail.com>
2020-03-02 21:25:25 +0600

    math build in function exercise.. 🗽📝

---
+++ 01 - JavaScript-Basics/mathFunc.js
@@ -0,0 +1,37 @@
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.round(5.7)); // 6
+console.log(Math.round(5.4)); // 5
+
+console.log(Math.pow(9, 2)); // 81
+
+console.log(Math.sqrt(81)); // 9
+
+console.log(Math.abs(-5.7)); // 5.7
+
+console.log(Math.ceil(5.4)); // 6
+
+console.log(Math.floor(6.7)); // 6
+
+console.log(Math.sin((90 * Math.PI) / 180)); // 1
+
+console.log(Math.cos((0 * Math.PI) / 180)); // 1
+
+console.log(Math.min(0, 900, 150, 30, 20, -8, -200)); // -200
+
+console.log(Math.max(0, 900, 150, 30, 20, -8, -200)); // 900
+
+console.log(Math.random()); // returns a random number
+
+// Math Properties (Constants)
+
+/**
+ * Math.E        // returns Euler's number
+ * Math.PI       // returns PI
+ * Math.SQRT2    // returns the square root of 2
+ * Math.SQRT1_2  // returns the square root of 1/2
+ * Math.LN2      // returns the natural logarithm of 2
+ * Math.LN10     // returns the natural logarithm of 10
+ * Math.LOG2E    // returns base 2 logarithm of E
+ * Math.LOG10E   // returns base 10 logarithm of E
+ */
