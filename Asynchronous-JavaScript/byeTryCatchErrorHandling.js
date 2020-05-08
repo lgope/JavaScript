024b9e1d6bd8e27fcfc8f8de067c2c2653b940e9
lgope <ilakshmangope@gmail.com>
2020-05-08 21:29:21 +0600

    async await error handling without try catch 🚀

--- Asynchronous-JavaScript/byeTryCatchErrorHandling.js
+++ Asynchronous-JavaScript/byeTryCatchErrorHandling.js
@@ -1,9 +1,8 @@
 // catchAwait.js
-const catchAwait = promise => (
+const catchAwait = promise =>
   promise
     .then(data => ({ data, error: null }))
-    .catch(error => ({ error, data: null }))
-);
+    .catch(error => ({ error, data: null }));
 
 module.exports = catchAwait;
 
@@ -17,6 +16,31 @@ const allItems = async () => {
     // code
   }
   console.error(error);
-}
+};
 
 allItems();
+
+/**
+ * Another way
+ */
+
+// catchAsync.js
+module.exports = fn => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
+// createOne.js
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
