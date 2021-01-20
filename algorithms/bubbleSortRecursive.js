099355e3f93d6ed961007c4b149e6c0fa0acd8ff
Lakshman <ilakshmangope@gmail.com>
2021-01-20 15:18:52 +0600

    Update bubbleSortRecursive.js

--- algorithms/bubbleSortRecursive.js
+++ algorithms/bubbleSortRecursive.js
@@ -7,28 +7,18 @@ function bubbleSort(arr, iteration) {
 
 
 function bubbleSwap(arr, i, j, n) {
-
   if (j >= n) return;
 
   // swap
   if (arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
 
-
   bubbleSwap(arr, i + 1, j + 1, n);
-}
-
-
-function sort(arr) {
-  const n = arr.length;
-
-  if (n <= 1) return;
-
-  bubbleSort(arr, n);
-
 }
 
 const arr = [1, 8, 6, 5, 4, 9];
 
-sort(arr);
+if (arr.length >= 1) bubbleSort(arr, arr.length)
 
 console.log(arr)
+
+// tc => O(n^2)
