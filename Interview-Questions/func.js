88b4884d5c017dce47e665c2d1e52d154ea4e86a
Lakshman <ilakshmangope@gmail.com>
2021-04-24 22:08:22 +0600

    Update func.js

--- Interview-Questions/func.js
+++ Interview-Questions/func.js
@@ -9,3 +9,28 @@ foo();
 
 console.log(a);
 console.log(b);
+
+
+// output
+function createIncrement() {
+	let count = 0;
+
+	function increment() {
+		count++;
+	}
+
+	let message = `Count is ${count}`;
+	function log() {
+		console.log(message);
+	}
+
+	return [increment, log];
+}
+
+const [increment, log] = createIncrement();
+
+increment();
+increment();
+increment();
+
+log();
