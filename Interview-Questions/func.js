c726aaf969ae8e4c4a9846b7544b50bbf8084642
Lakshman <ilakshmangope@gmail.com>
2021-10-24 12:52:32 +0600

    Update func.js

--- Interview-Questions/func.js
+++ Interview-Questions/func.js
@@ -34,3 +34,24 @@ increment();
 increment();
 
 log();
+
+// Console sequence output?
+function run () {
+  const promise = new Promise((resolve) => {
+    resolve('promise');
+  })
+
+  setTimeout(() => {
+   console.log('setTimeout'); 
+  });
+
+  promise.then(res => console.log(res))
+
+  console.log('log');
+}
+
+run();
+
+// How does the Javascript interprets the following code?
+console.log(x);
+var x = 100;
