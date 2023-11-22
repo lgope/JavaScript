c34b09f9df6f1a2283578efbb529c612a4305be4
Lakshman Gope <ilakshmangope@gmail.com>
2023-11-22 12:29:09 +0600

    Create output.js

---
+++ Interview-Questions/output.js
@@ -0,0 +1,5 @@
+// 1) What is the output of 10+20+"30" in JavaScript?
+//Ans: 3030 because 10+20 will be 30. If there is numeric value before and after +, it treats as binary + (arithmetic operator).
+  
+// 2) What is the output of "10"+20+30 in JavaScript?
+// Ans: 102030 because after a string all the + will be treated as string concatenation operator (not binary +).
