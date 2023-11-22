df85cf90b6cb8798de4c730ffbe99213a55e5a00
Lakshman Gope <ilakshmangope@gmail.com>
2023-11-22 23:11:39 +0600

    Update output.js

--- Interview-Questions/output.js
+++ Interview-Questions/output.js
@@ -3,3 +3,20 @@
   
 // 2) What is the output of "10"+20+30 in JavaScript?
 // Ans: 102030 because after a string all the + will be treated as string concatenation operator (not binary +).
+
+// 3) Output?
+// Syncronous
+[1, 2, 3, 4].forEach((i) => {
+  console.log(i)
+})
+
+// Asynchronous
+function asyncForEach(array, cb) {
+  array.forEach(() => {
+    setTimeout(cb, 0);
+  })
+}
+
+asyncForEach([1, 2, 3, 4], (i) => {
+  console.log(i);
+})
