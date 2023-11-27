b77b5af004fadeea152795e9c430d1adc23c3c0e
lgope <ilakshmangope@gmail.com>
2023-11-27 19:56:43 +0600

    closure question and solution

--- Interview-Questions/output.js
+++ Interview-Questions/output.js
@@ -185,3 +185,44 @@ function runWhileLoopForNSeconds(sec) {
     now = Date.now();
   }
 }
+
+// Write a function where suppose you want to use a variable for counting something, and you want this counter to be available to all functions.
+
+// Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+// The counter should now be 3
+
+// Solve
+const add = (function () {
+  let counter = 0;
+  return function () {
+    counter += 1;
+    return counter;
+  };
+})();
+
+add();
+add();
+add(); // the counter is now 3
+
+// Explained
+// The variable add is assigned to the return value of a self-invoking function.
+
+// The self-invoking function only runs once. It sets the counter to zero (0), and returns a function expression.
+
+// This way add becomes a function. The "wonderful" part is that it can access the counter in the parent scope.
+
+// This is called a JavaScript closure. It makes it possible for a function to have "private" variables.
+
+// The counter is protected by the scope of the anonymous function, and can only be changed using the add function.
